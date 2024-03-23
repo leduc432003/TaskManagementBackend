38d80c5fe0608679dce0e0f96fb510d97416c7c7
leduc432003 <anhducle4433@gmail.com>
2024-03-23 08:39:33 +0700

    commit

--- taskuserservice/src/main/java/com/duc/taskuserservice/config/ApplicationConfiguration.java
+++ taskuserservice/src/main/java/com/duc/taskuserservice/config/ApplicationConfiguration.java
@@ -39,7 +39,7 @@ public class ApplicationConfiguration {
             @Override
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration cfg = new CorsConfiguration();
-                cfg.setAllowedOrigins(Collections.singletonList("*"));
+                cfg.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
                 cfg.setAllowedMethods(Collections.singletonList("*"));
                 cfg.setAllowCredentials(true);
                 cfg.setAllowedHeaders(Collections.singletonList(("*")));
